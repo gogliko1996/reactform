3a36e19d7ea1b959227455ddde13a3d908abd4a9
gogliko1996 <chakiduli@gmail.com>
2022-12-16 23:48:47 +0400

    form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,169 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+import "./App.css";
 
 function App() {
+  const nameRef = useRef();
+  const firstnameRef = useRef();
+  const emailRef = useRef();
+  const ageRef = useRef();
+
+  const [isupdate, setIsupdate] = useState(false);
+  //users array
+  const [users, setusers] = useState([]);
+  //chekbox input
+  const [inputChecked, setInputChecked] = useState("");
+  //add user value name
+  const [inputValue, setInputValue] = useState({
+    name: "",
+    firstname: "",
+    email: "",
+    age: "",
+    gender: "",
+  });
+  //inputvalue objeqt values
+  const oNinputChange = () => {
+    const newInputName = { ...inputValue };
+
+    newInputName.name = nameRef.current.value;
+    newInputName.firstname = firstnameRef.current.value;
+    newInputName.email = emailRef.current.value;
+    newInputName.age = ageRef.current.value;
+    
+    setInputValue(newInputName);
+  };
+  //checkbox clic
+  const onCheckbox = (e) => {
+    setInputChecked(e.target.value);
+  };
+
+  const addUser = (event) => {
+    event.preventDefault();
+    //new user objeqt
+    if (!isupdate) {
+      const user = {
+        id: Math.floor(Math.random() * (10 - 1 + 1) + 1),
+        name: nameRef.current.value,
+        firstname: firstnameRef.current.value,
+        email: emailRef.current.value,
+        age: ageRef.current.value,
+        gender: inputChecked,
+      };
+
+      const newUser = [...users, user];
+
+      setusers(newUser);
+    } else {
+      const newUsers = [...users];
+
+      for (let index in newUsers) {
+        if (newUsers[index].id === inputValue.id) {
+          newUsers[index] = inputValue;
+          break;
+        }
+      }
+
+      setusers(newUsers);
+
+    }
+    ///inputvalues objeqt value clear
+    const newInputName = { ...inputValue };
+
+    newInputName.name = "";
+    newInputName.firstname = "";
+    newInputName.email = "";
+    newInputName.age = "";
+
+    setInputValue(newInputName);
+    setInputChecked("");
+    setIsupdate(false);
+  };
+
+  const deleteUser = (id) => {
+    const newUser = [...users];
+
+    const filterUser = newUser.filter((people) => {
+      return people.id !== id;
+    });
+
+    setusers(filterUser);
+
+  };
+
+  const updateUser = (id) => {
+    setIsupdate(true);
+    const newUser = [...users];
+    let newInputName = { ...inputValue };
+
+    newUser.map((people) => {
+      if (people.id === id) {
+        newInputName = people;
+      }
+    });
+
+    setInputValue(newInputName);
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <input
+          value={inputValue.name}
+          ref={nameRef}
+          onChange={oNinputChange}
+          placeholder="name"
+        />
+        <input
+          value={inputValue.firstname}
+          ref={firstnameRef}
+          onChange={oNinputChange}
+          placeholder="firstname"
+        />
+        <input
+          value={inputValue.email}
+          ref={emailRef}
+          onChange={oNinputChange}
+          placeholder="email"
+        />
+        <input
+          type="number"
+          value={inputValue.age}
+          ref={ageRef}
+          onChange={oNinputChange}
+          placeholder="age"
+        />
+        <input
+          type="checkbox"
+          value="women"
+          onChange={onCheckbox}
+          checked={inputChecked === "women"}
+        />{" "}
+        {"  "} women
+        <input
+          type="checkbox"
+          value="man"
+          onChange={onCheckbox}
+          checked={inputChecked === "man"}
+        />{" "}
+        {"  "} man
+        <button onClick={addUser}> {isupdate ? "update" : "add"} </button>
+      </form>
+
+      <div className="users">
+        {users.map((people) => {
+          return (
+            <ul key={people.id}>
+              <li> name: {people.name}</li>
+              <li> firstname: {people.firstname}</li>
+              <li> email: {people.email}</li>
+              <li> age: {people.age}</li>
+              <li> gender: {people.gender}</li>
+              <button onClick={() => deleteUser(people.id)}>delete</button>
+              <button onClick={() => updateUser(people.id)}>update</button>
+            </ul>
+          );
+        })}
+      </div>
     </div>
   );
 }
